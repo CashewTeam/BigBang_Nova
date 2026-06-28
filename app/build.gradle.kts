6f5474d43aea3a753b778e4b93e9c79a474b94e5
CashewTeam <cashewteam@outlook.com>
2026-06-29 01:10:39 +0800

    Allow launcher queries and bump version to 1.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cashewteam.novatext.android"
         minSdk = 29
         targetSdk = 34
-        versionCode = 93
-        versionName = "1.8.4"
+        versionCode = 95
+        versionName = "1.8.6"
 
         ndk {
             abiFilters += "arm64-v8a"
