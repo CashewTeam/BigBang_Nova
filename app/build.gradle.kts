767258b407334be94a829ccec305361aaf654979
CashewTeam <cashewteam@outlook.com>
2026-07-01 02:51:40 +0800

    Bump version and tighten settings top padding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cashewteam.novatext.android"
         minSdk = 29
         targetSdk = 34
-        versionCode = 158
-        versionName = "1.10.48"
+        versionCode = 159
+        versionName = "1.10.49"
 
         ndk {
             abiFilters += "arm64-v8a"
