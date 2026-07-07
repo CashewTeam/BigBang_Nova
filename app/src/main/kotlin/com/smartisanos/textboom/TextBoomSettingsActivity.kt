cace3a5ceb20464c8e2b30d13c111d3b3533c33d
SkyShadowHero <a1233456333@163.com>
2026-07-07 18:03:50 +0800

    feat: 删除主页surface里OptionRow的圆角

--- app/src/main/kotlin/com/smartisanos/textboom/TextBoomSettingsActivity.kt
+++ app/src/main/kotlin/com/smartisanos/textboom/TextBoomSettingsActivity.kt
@@ -2105,7 +2105,6 @@ private fun OptionRow(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clip(RoundedCornerShape(18.dp))
             .clickable(onClick = onClick)
             .padding(horizontal = 16.dp, vertical = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
